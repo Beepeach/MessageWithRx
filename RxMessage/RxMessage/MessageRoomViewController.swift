9d28cb9a53b267e4d2d235701b031180de531a15
Beepeach <eh7457303j@gmail.com>
2022-04-26 15:33:04 +0900

    Feat: MessageRoomVC의 tableView에 dummy data가 표시되도록 구현

--- RxMessage/RxMessage/MessageRoomViewController.swift
+++ RxMessage/RxMessage/MessageRoomViewController.swift
@@ -6,13 +6,54 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
 
 class MessageRoomViewController: UIViewController {
-
+    private let bag: DisposeBag = DisposeBag()
+    
+    private let dummyData: Observable<[Message]> = Observable.of([
+        Message(type: .text, who: "me", body: "Hello RxSwift!!"),
+        Message(type: .image, who: "you", body: "imageURL?? 뭐가 와야할까"),
+        Message(type: .text, who: "me", body: "Good")
+    ])
+    
+    private var messageTableView: UITableView = UITableView().then {
+        $0.backgroundColor = .systemTeal
+    }
+    
+    private var cell: UITableViewCell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        setupUI()
+        bindMessageTableView()
+    }
+    
+    private func setupUI() {
+        setupMessageTableViewUI()
+    }
+    
+    private func setupMessageTableViewUI() {
+        view.addSubview(messageTableView)
+        
+        messageTableView.snp.makeConstraints {
+            $0.top.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    private func bindMessageTableView() {
+        dummyData.bind(to: messageTableView.rx.items) { tableView, index, message in
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            
+            cell.textLabel?.text = message.body
+            
+            return cell
+        }
+        .disposed(by: bag)
     }
 }
 
