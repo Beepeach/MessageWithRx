b00c4c4f054f319147ad4debc9906e487048b0fd
Beepeach <eh7457303j@gmail.com>
2022-05-18 18:14:10 +0900

    Feat: 메세지 입력창 UI코드 추가

--- RxMessage/RxMessage/MessageRoom/MessageRoomViewController.swift
+++ RxMessage/RxMessage/MessageRoom/MessageRoomViewController.swift
@@ -17,7 +17,10 @@ class MessageRoomViewController: UIViewController, ViewModelBindableType {
     // MARK: Private properties
     private let bag: DisposeBag = DisposeBag()
     private var messageTableView: UITableView = MessageRoomViewController.makeMessageTableView()
+    private let inputContainerView: UIView = MessageRoomViewController.makeInputContainerView()
     private let inputField: UITextField = MessageRoomViewController.makeInputField()
+    private let sendButton: UIButton = MessageRoomViewController.makeSendButton()
+
     
     
     // MARK: Properties
@@ -62,6 +65,12 @@ class MessageRoomViewController: UIViewController, ViewModelBindableType {
             .compactMap { self.getImage(from: $0.body) }
             .bind(to: viewModel.detailImageAction.inputs)
             .disposed(by: bag)
+        
+        //        NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+        //            .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect }
+        //            .map { $0.height }
+        
+        
     }
     
     private func getImage(from url: String) -> UIImage? {
@@ -90,19 +99,8 @@ class MessageRoomViewController: UIViewController, ViewModelBindableType {
 
 
 extension MessageRoomViewController {
-    static func makeMessageTableView() -> UITableView {
-        let tableView = UITableView().then {
-            $0.register(TextTableViewCell.classForCoder(), forCellReuseIdentifier: TextTableViewCell.identifier)
-            $0.register(ImageTableViewCell.classForCoder(), forCellReuseIdentifier: ImageTableViewCell.identifier)
-            
-            $0.separatorStyle = .none
-        }
-        
-        return tableView
-    }
-    
     private func setupUI() {
-        setupInputField()
+        setupInputContainerView()
         setupMessageTableViewUI()
     }
     
@@ -111,28 +109,79 @@ extension MessageRoomViewController {
         
         messageTableView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
-            
-            $0.bottom.equalTo(inputField.snp.top)
+            $0.bottom.equalTo(inputContainerView.snp.top)
         }
+    }
+    
+    private func setupInputContainerView() {
+        view.addSubview(inputContainerView)
+        
+        inputContainerView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.1)
+        }
+        
+        inputContainerView.do {
+            setupSendButton(in: $0)
+            setupInputField(in: $0)
+        }
+    }
+    
+    private func setupSendButton(in container: UIView) {
+        container.addSubview(sendButton)
+        
+        sendButton.snp.makeConstraints {
+            $0.height.width.equalTo(33)
+            $0.top.equalToSuperview().offset(10)
+            $0.trailing.equalToSuperview().offset(-10)
+        }
+    }
+    
+    private func setupInputField(in container: UIView) {
+        container.addSubview(inputField)
+        
+        inputField.snp.makeConstraints {
+            $0.centerY.equalTo(sendButton)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalTo(sendButton.snp.leading).offset(-10)
+        }
+    }
+    static func makeMessageTableView() -> UITableView {
+        let tableView = UITableView().then {
+            $0.register(TextTableViewCell.classForCoder(), forCellReuseIdentifier: TextTableViewCell.identifier)
+            $0.register(ImageTableViewCell.classForCoder(), forCellReuseIdentifier: ImageTableViewCell.identifier)
+            
+            $0.separatorStyle = .none
+            $0.contentInset.bottom = 60
+        }
+        
+        return tableView
+    }
+    
+    static func makeInputContainerView() -> UIView {
+        let containerView = UIView().then {
+            $0.backgroundColor = .systemGray5
+        }
+        
+        return containerView
     }
     
     static func makeInputField() -> UITextField {
         let textField = UITextField().then {
-            $0.borderStyle = .line
+            $0.borderStyle = .roundedRect
+            $0.placeholder = "메시지를 입력하세요."
             $0.textColor = .black
-            $0.backgroundColor = .brown
         }
         
         return textField
     }
     
-    private func setupInputField() {
-        view.addSubview(inputField)
-        
-        inputField.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview()
-            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+    static func makeSendButton() -> UIButton {
+        let button = UIButton().then {
+            $0.setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
         }
+        
+        return button
     }
 }
 
